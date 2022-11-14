22f4ce6d3fd123d8c4267dc7642ca892f124e0ef
zawat-tin <contactaizawa@icloud.com>
2022-11-14 14:44:14 +0900

    add title

---
+++ zawatin_practice_games/Title.cpp
@@ -0,0 +1,16 @@
+﻿#include "Title.hpp"
+
+void Title::debug() const {
+	Print << U"Title";
+}
+
+Title::Title(const InitData& init) : IScene(init) {
+	Scene::SetBackground(Palette::White);
+	debug();
+}
+
+void Title::update() {
+}
+
+void Title::draw() const {
+}
---
+++ zawatin_practice_games/Title.hpp
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "Base.hpp"
+
+class Title : public App::Scene {
+private:
+	void debug() const ;
+public:
+
+	Title(const InitData& init);
+
+	void update() override;
+
+	void draw() const override;
+};
